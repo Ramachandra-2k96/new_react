77c92bf5cd5a2065df35c6f5d33495f8e58ad5d3
Ramachandra-2k96 <ramachandraudupa.sangamone@gmail.com>
2024-06-07 20:01:48 +0530

    first commit

---
+++ myfrontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/static/frontend/',
+  build: {
+    outDir: '../myproject/static/frontend',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+})
